a5a635f68fa412f948477eed86eada70e7a6b614
egue harris <harrisegue9@gmail.com>
2026-03-25 15:50:33 +0100

    env error fix

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,7 +3,7 @@ import '@/models/User';
 import '@/models/Chat';
 import '@/models/Message';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/oasis';
+const MONGODB_URI = process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable');
